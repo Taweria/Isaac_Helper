c33e0ead2f08461a1a8b78fcce77b2f5fc52bc6f
Taweria <elodieali.pro@gmail.com>
2024-02-21 09:31:13 +0100

    First fetch

---
+++ js/call.js
@@ -0,0 +1,19 @@
+// Fetch JSON data
+fetch('../json/items.json')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+
+    data.forEach(item => {
+        console.log(item.nom);
+        console.log(item.description);
+    });
+
+  })
+  .catch(error => {
+    console.error('There was a problem with the fetch operation:', error);
+  });
